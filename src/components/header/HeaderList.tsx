7e007e05f411326b141c7baafa83bdd76c5bd223
otesic <qkrcjffus@naver.com>
2022-12-08 15:04:28 +0900

    Feat: login modal 80% nav 80% #15

--- src/components/header/HeaderList.tsx
+++ src/components/header/HeaderList.tsx
@@ -1,23 +1,45 @@
 import Nav from "react-bootstrap/Nav";
 import { useNavigate } from "react-router-dom";
 // import HeaderList from './HeaderList';
+import { useState } from "react";
+import { useEffect } from "react";
+import "./Header.css";
 type Props = {
   value: string;
   name: string;
 };
 const HeaderList = ({ value, name }: Props) => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const updateScroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", updateScroll);
+  });
   const navigate = useNavigate();
   const linkTo = (path: string) => {
     navigate(path);
   };
   return (
-    <Nav.Link
-      onClick={() => {
-        linkTo(`/${value}`);
+    <div
+      style={{
+        fontSize: "16px",
+        margin: "0",
+        color: "white",
+        justifyContent: "space-evenly",
       }}
+      className={
+        scrollPosition < 100 ? "original_header_list" : "change_header_list"
+      }
     >
-      {name}
-    </Nav.Link>
+      <a
+        onClick={() => {
+          linkTo(`/${value}`);
+        }}
+      >
+        {name}
+      </a>
+    </div>
   );
 };
 export default HeaderList;

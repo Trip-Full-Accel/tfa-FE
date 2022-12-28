7bf21530dc2decc98ebd7e8cc71410c0e759aa3d
PARKGWANGDUCK <qkrqkqhek@gmail.com>
2022-12-28 10:49:46 +0900

    slide-famous_places

--- src/components/slide/DaeguSlide.tsx
+++ src/components/slide/DaeguSlide.tsx
@@ -21,44 +21,124 @@ export default class DaeguSlide extends Component {
         <AreaName></AreaName>
         <div>
           <MainSlideDiv>
-            <div
-              className="busan"
-              style={{
-                width: "80%",
-              }}
-            >
+            <div className="place">
               <Slider {...settings}>
-                <div style={{ width: "1000" }}>
-                  <img src="/img/busan/busan1.jpeg" />
-                  <h3>대구</h3>
+                <div>
+                  <ImgTag src="/img/daegu/daegu1.png" />
+                  <br></br>
+                  <h2>계산성당</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    계산성당은 서울 명동성당, 전주 전동성당과 함께 우리나라 3대
+                    성당 중 한 곳이며 세 성당 모두 프와넬 신부가 설계하여 내부
+                    <br />
+                    구조가 비슷하다 하다. 대구 최초의 서양식 건물로 두 개의
+                    첨탑을 <br />
+                    가진 고딕 양식의 아름다운 건물이다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/seoul/seoul1.jpg" />
-                  <h3>대구</h3>
+                  <ImgTag src="/img/daegu/daegu2.png" />
+                  <br></br>
+                  <h2>수성못</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    수성구 두산동의 수성못은 도심 속 산책과 휴식 공간이다.
+                    <br />
+                    수성못은 주변 풍경이 아름답고 산책로와 부대시설이 잘 갖춰져
+                    있고 호수에서는 보트 놀이도 할 수 있고 수상 분수 쇼도 볼만
+                    해서 <br />
+                    연인들의 데이트 명소이다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/daegu/daegu1.jpg" />
-                  <h3>대구</h3>
+                  <ImgTag src="/img/daegu/daegu3.png" />
+                  <br></br>
+                  <h2>83타워</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    전망대에서는 대구 시내와 멀리 팔공산까지 한눈에 조망할 수
+                    <br />
+                    있다. 야경이 이름다운 스카이라운지는 연인들의 프러포즈
+                    장소로
+                    <br /> 유명하다. 123m에서 뛰어내리는 스카이점프는 이곳의
+                    명물이다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/daejeon/daejeon1.jpeg" />
-                  <h3>대구</h3>
+                  <ImgTag src="/img/daegu/daegu4.png" />
+                  <br></br>
+                  <h2>앞산공원</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    공원에는 계곡, 약수터, 산책로가 있고 낙동강 승전기념관 등
+                    <br />
+                    유적도 많고 편의시설로는 수영장, 도서관, 식물원 등 대구
+                    시민의 <br />
+                    휴식처이다. 케이블카를 타고 전망대에 오르면 대구 시내를
+                    <br />
+                    한눈에 조망할 수 있다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/busan/busan2.jpeg" />
-                  <h3>대구</h3>
+                  <ImgTag src="/img/daegu/daegu5.png" />
+                  <br></br>
+                  <h2>동화사</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    세계에서 가장 큰 석조불이라는 33m 높이의 통일약사대불은
+                    <br />
+                    이곳의 볼거리이다. 불교미술을 전시하는 성보박물관, 선을
+                    테마로 한 불교문화관, 사찰음식 체험관 등이 있어 불교문화를
+                    살펴보기 좋다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/seoul/seoul2.jpg" />
-                  <h3>대구</h3>
+                  <ImgTag src="/img/daegu/daegu6.png" />
+                  <br></br>
+                  <h2>비슬산군립공원</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    봄이면 고지 30만 평에 걸쳐 진달래꽃이 흐드러지게 피어 장관을
+                    연출한다. 매년 4∼5월에 참꽃축제를 열고, 민속놀이·먹을거리
+                    <br />
+                    장터 등의 부대 행사를 개최한다. 그 밖에 여름에는 안개 계곡,
+                    가을에는 단풍과 참억새, 겨울에는 얼음 동산이 유명하다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/daegu/daegu2.jpg" />
-                  <h3>대구</h3>
+                  <ImgTag src="/img/daegu/daegu7.png" />
+                  <br></br>
+                  <h2>국채보상운동기념공원</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    대구에서 처음 시작된 국채보상운동의 정신이 깃든 도심 속
+                    <br />
+                    쉼터이다. 잔디광장과 녹지 공간, 오솔길, 분수, 정자 등을
+                    <br />
+                    갖추고 있다. 곳곳에 벤치와 휴식 공간이 많아
+                    <br /> 시민은 물론 여행객들도 많이 찾는다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/daejeon/daejeon2.jpeg" />
-                  <h3>대구</h3>
+                  <ImgTag src="/img/daegu/daegu8.png" />
+                  <br></br>
+                  <h2>이월드</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    놀이기구와 전시공간이 공존하는 유럽식 도시공원으로 남녀노소
+                    누구나 즐길 수 있는 공간이다. 별빛축제 기간에는 더욱 화려한
+                    밤을 <br />
+                    즐길 수 있다.
+                  </h4>
                 </div>
               </Slider>
               <br></br>
@@ -73,4 +153,11 @@ export default class DaeguSlide extends Component {
 const MainSlideDiv = styled.div`
   display: flex;
   flex-wrap: wrap;
+`;
+
+const ImgTag = styled.img`
+  width: 500px;
+  height: 350px;
+  margin: 10px
+  margin: 0;
 `;
--- src/components/slide/SeoulSlide.tsx
+++ src/components/slide/SeoulSlide.tsx
@@ -21,44 +21,125 @@ export default class SeoulSlide extends Component {
         <AreaName></AreaName>
         <div>
           <MainSlideDiv>
-            <div
-              className="busan"
-              style={{
-                width: "80%",
-              }}
-            >
+            <div className="place">
               <Slider {...settings}>
-                <div style={{ width: "1000" }}>
-                  <img src="/img/busan/busan1.jpeg" />
-                  <h3>서울</h3>
+                <div>
+                  <ImgTag src="/img/seoul/seoul1.png" />
+                  <br></br>
+                  <h2>북한산 둘레길</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    사람과 자연이 하나가 되어 걸을 수 있도록 물길, 흙길, 숲길 등
+                    <br />
+                    21가지 다양한 테마를 담았죠. 총 길이 71.5km의 둘레길 전체를
+                    <br />
+                    완주하며 스탬프를 모으는 것도 둘레길을 즐기는 좋은 방법이다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/seoul/seoul1.jpg" />
-                  <h3>서울</h3>
+                  <ImgTag src="/img/seoul/seoul2.png" />
+                  <br></br>
+                  <h2>한강공원</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    기본적인 레포츠 시설을 갖추고 있고, 전망대나 아름다운 분수
+                    등<br /> 볼거리가 많은 공원은 물론 생태 캠핑장이 마련된 곳도
+                    있어요.
+                    <br /> 한강공원은 다양한 매력으로 낮에도 밤에도 사람들의
+                    <br />
+                    발길이 끊이지 않는 곳이다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/daegu/daegu1.jpg" />
-                  <h3>서울</h3>
+                  <ImgTag src="/img/seoul/seoul3.png" />
+                  <br></br>
+                  <h2>북촌 한옥 마을</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    북촌은 조선시대부터 양반층의 주거지였지만 서울의 개발과 함께
+                    한옥을 보존하기 위해 계획적으로 조성된 한옥 주거지다. 이
+                    지역의 한옥은 고택과는 달리 한옥의 특징을 살리면서도
+                    현대적인 기술이나 장식적 요소가 더해져 있다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/daejeon/daejeon1.jpeg" />
-                  <h3>서울</h3>
+                  <ImgTag src="/img/seoul/seoul4.png" />
+                  <br></br>
+                  <h2>창경궁 야간개장</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    창경궁 야간개장은 청사초롱과 함께 하기 때문에 더욱
+                    <br />
+                    낭만적이다. 해가 저문 뒤 청사초롱을 들고 창경궁을 거닐면
+                    <br />
+                    마치 조선시대로 돌아간 것 같은 특별한 경험을 할 수 있다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/busan/busan2.jpeg" />
-                  <h3>서울</h3>
+                  <ImgTag src="/img/seoul/seoul5.png" />
+                  <br></br>
+                  <h2>운현궁 양관</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    운현궁 양관은 운현궁 바로 옆 덕성여대 내에 있는 근대
+                    <br />
+                    건축물이다. 아름다운 건물은 드라마 ‘도깨비’의 배경으로도
+                    <br />
+                    유명한데, 평일에는 대학 관계자 외에는 출입이 금지되어 있으니
+                    <br />
+                    주말을 이용해 방문해 보자.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/seoul/seoul2.jpg" />
-                  <h3>서울</h3>
+                  <ImgTag src="/img/seoul/seoul6.png" />
+                  <br></br>
+                  <h2>롯데월드타워</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    2016년에 완공된 롯데월드타워는 555m의 현대적인 고층
+                    <br /> 빌딩으로 맨 꼭대기의 전망대에서 360도로 서울의 전경을
+                    볼 수<br /> 있는 게 특징이다. 아쿠아리움 및 콘서트홀, 영화관
+                    등 다양한
+                    <br />
+                    엔터테인먼트와 쇼핑, 음식 등을 한 자리에서 즐기실 수 있다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/daegu/daegu2.jpg" />
-                  <h3>서울</h3>
+                  <ImgTag src="/img/seoul/seoul7.png" />
+                  <br></br>
+                  <h2>이태원</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    여러 외국 공관을 중심으로 다양한 나라의 사람들이 모이면서
+                    <br />
+                    다채로운 외국 문화를 접할 수 있는 독특한 분위기의 동네가
+                    <br />
+                    형성되었다. 이태원에서라면 서울을 떠나지 않고도
+                    <br />전 세계 다양한 사람을 만날 수가 있다.
+                  </h4>
                 </div>
                 <div>
-                  <img src="/img/daejeon/daejeon2.jpeg" />
-                  <h3>서울</h3>
+                  <ImgTag src="/img/seoul/seoul8.png" />
+                  <br></br>
+                  <h2>봉은사</h2>
+                  <br></br>
+                  <br></br>
+                  <h4>
+                    추사 김정희가 쓴 현판이 걸린 판전에는 화엄경, 금강경 등
+                    <br />
+                    13가지의 불경 경판 3,479판이 보관되어 있다.
+                    <br /> 매년 음력 9월 9일에는 사부대중이 함께 경판을 머리에
+                    이고
+                    <br /> 법성게를 독송하면서 법계도를 따라 행진하는
+                    <br /> 정대불사라는 의식이 행해진다.
+                  </h4>
                 </div>
               </Slider>
               <br></br>
@@ -73,4 +154,11 @@ export default class SeoulSlide extends Component {
 const MainSlideDiv = styled.div`
   display: flex;
   flex-wrap: wrap;
+`;
+
+const ImgTag = styled.img`
+  width: 500px;
+  height: 350px;
+  margin: 10px
+  margin: 0;
 `;
